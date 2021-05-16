4aef690e22af29cb081a330224109991bcc6e86a
Chiagozie Robert-Maduekwe <chiagozier@yahoo.com>
2021-05-15 20:24:22 -0400

    made changes

--- app/src/main/java/com/example/loginactivity/LoginAdapter.kt
+++ app/src/main/java/com/example/loginactivity/LoginAdapter.kt
@@ -1,12 +1,27 @@
 package com.example.loginactivity
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
-class LoginAdapter(context: Context, facts:List<LoginModel>):ArrayAdapter<LoginModel>(context, resource, 0, facts) {
+class LoginAdapter(context: Context, facts:List<LoginModel>):ArrayAdapter<LoginModel>(context, 0, facts) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+        var view = convertView
+        val fact = getItem(position)
+        if (view ==null) {
+            view = LayoutInflater.from(parent.context).inflate(R.layout.fact_item, parent, false)
+        }
+        val logo = view?.findViewById<ImageView>(R.id.imageLogo)
+        val name = view?.findViewById<TextView>(R.id.nameTv)
+        fact?.logo?.let {
+            logo?.setImageResource(fact?.logo)
+        }
+        name?.text = fact?.name
+
+        return view!!
     }
 }
--- app/src/main/java/com/example/loginactivity/MainActivity.kt
+++ app/src/main/java/com/example/loginactivity/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.loginactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding:ActivityMainBinding? = null
+    var adapter:LoginAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setUpFact()
+    }
+
+    private fun setUpFact(){
+        adapter = LoginAdapter(this, DummyData.cityFacts)
+        binding?.factListView?.adapter = adapter
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
